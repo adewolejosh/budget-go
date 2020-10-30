0fbb0fcf4bc060ba1deceaef6a349a33dde2e88a
Joshua Adewole <adewole.josh@gmail.com>
2020-10-30 18:40:35 +0100

    First deploy to netflify

---
+++ dist/precache-manifest.40045a5680807670dccec50fe8a15169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8b4aa76aa058c0670c",
+    "url": "/css/app.a84c4a52.css"
+  },
+  {
+    "revision": "03f578894acb2e377fd02792d192e774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001779cba91485d6a847",
+    "url": "/js/about.6881a105.js"
+  },
+  {
+    "revision": "af8b4aa76aa058c0670c",
+    "url": "/js/app.be5e348f.js"
+  },
+  {
+    "revision": "61f5d8a7c03392d805fe",
+    "url": "/js/chunk-vendors.0ec4d5f0.js"
+  },
+  {
+    "revision": "336207650d261d56e6099ffe82a751fd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
